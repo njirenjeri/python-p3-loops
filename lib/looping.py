5285376f3ef29018b8e82f8394eb48919505ee6b
Njire Njeri <njirenjeri@gmail.com>
2025-02-18 18:53:18 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
+    for i in range(11):
+        print(i)
+        print('Happy New Year!')
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [x ** 2 for x in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
     pass
